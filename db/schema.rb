aac159ea63fe18b049e55600d5cec4176fd03b6e
Paresh10 <paresh.sharma10@gmail.com>
2021-09-25 17:00:11 -0500

    Created poker db to expand this app further

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_21_014539) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "faces", force: :cascade do |t|
     t.string "value"
     t.string "url"
